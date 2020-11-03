31b297e965d39648c1b0c3d33f457d3d71b835a0
Calvin Kinateder <calvinkinateder1@yahoo.com>
2020-11-03 13:54:46 -0500

    actually working...keeps getting blocked though"

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ class SupremeBot:
             print(out)
 
     def init_browser(self):
-        self.b = Browser('chrome')
+        self.b = Browser('chrome', incognito=True)#,headless=True)
     
     def find_product(self):
         url = '{}{}{}'.format(self.base, self.shop_ext, self.info['category'])
@@ -34,34 +34,64 @@ class SupremeBot:
                 temp_link.append(i[0])
         self.debug_console(temp_link)
         #self.final_link = list(set([x for x in temp_link if temp_link.count(x) == 2]))
-        self.final_link = temp_link[0]
+        self.final_link = 'shop/tops-sweaters/x6vo2ze7i/nwszmv5ot'#temp_link[0]
         print(self.final_link)
 
     def visit_site(self):
         self.b.visit('{}{}'.format(self.base, str(self.final_link)))
         self.b.find_option_by_text(self.info['size']).click()
-        self.b.find_by_value('add to basket').click()
+        self.b.find_by_value('add to cart').click()
+        self.b.visit('{}{}'.format(self.base, self.checkout_ext))
+    
+    def checkout_func(self):
+        self.b.visit('{}{}'.format(self.base, self.checkout_ext))
+        #self.debug_console('{}{}'.format(self.base, self.checkout_ext))
+        #self.debug_console(self.b.execute_script("return document.documentElement.outerHTML;"))
 
+        self.b.fill('order[billing_name]', self.info['namefield'])
+        self.b.fill('order[email]', self.info['emailfield'])
+        self.b.fill('order[tel]', self.info['phonefield'])
+
+        self.b.fill('order[billing_address]', self.info['addressfield'])
+        self.b.fill('order[billing_city]', self.info['city'])
+        self.b.select('order[billing_state]', self.info['state'])
+        self.b.fill('order[billing_zip]', self.info['zip'])
+        self.b.select('order[billing_country]', self.info['country'])
+
+        #self.b.select('credit_card[type]', self.info['card'])
+        self.b.fill('riearmxa', self.info['number'])
+        self.b.select('credit_card[month]', self.info['month']) #wierd name
+        self.b.select('credit_card[year]', self.info['year'])
+        self.b.fill('credit_card[meknk]', self.info['ccv']) #wierd name
+        self.b.find_by_css('.terms').click()
+        #self.b.find_by_value('process payment').click()
+
+    def main(self):
+        self.init_browser()
+        self.find_product()
+        self.visit_site()
+        self.checkout_func()
 
 
 if __name__ == '__main__':
     INFO = {
-            'product': 'S/S Pocket Tee',
-            'color': 'Black',
+            'product': 'Thermal Zip Up Hooded Sweatshirt',
+            'color': 'Tangerine',
             'size': 'Medium',
-            'category': 'tops_sweaters',
+            'category': 'sweatshirts',
             'namefield': 'example',
             'emailfield': 'example@example.com',
             'phonefield': 'XXXXXXXXXX',
             'addressfield': 'example road',
-            'city': 'example',
-            'zip': '42155',
-            'country': 'US',
-            'card': 'visa',
+            'city': 'cincinnati',
+            'state': 'OH',
+            'zip': '42152',
+            'country': 'USA',
+            'card': 'Credit Card',
             'number': '1234123412341234',
             'month': '09',
             'year': '2020',
             'ccv': '123'
             }
     bot = SupremeBot(True,**INFO)
-    bot.find_product()
+    bot.main()
