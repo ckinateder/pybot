8ba6f0224390b7bc3a2b4da696a4c7bf7a7e2666
Calvin Kinateder <calvinkinateder1@yahoo.com>
2020-11-03 13:13:30 -0500

    possibly working get values

--- bot.py
+++ bot.py
@@ -3,26 +3,46 @@ import bs4
 from splinter import Browser
 
 class SupremeBot:
-    def __init__(self, **info):
-        self.base = "http://supremenewyork.com/"
+    def __init__(self, debug=False, **info):
+        self.base = "https://www.supremenewyork.com/"
         self.shop_ext = "shop/all/"
         self.checkout_ext = "checkout/"
         self.info = info
+        self.debug = debug
+
+    def debug_console(self, out):
+        if self.debug:
+            print(out)
 
     def init_browser(self):
-        self.b = Browser()
+        self.b = Browser("chrome")
     
     def find_product(self):
-        r = requests.get("{}{}{}".format(self.base, self.shop, self.info["category"])).text
+        url = "{}{}{}".format(self.base, self.shop_ext, self.info["category"])
+        self.debug_console(url)
+        r = requests.get(url, timeout=5).text
         soup = bs4.BeautifulSoup(r, 'lxml')
+        #self.debug_console(soup)
+        temp_tuple = []
+        temp_link = []
 
+        for link in soup.find_all("a", href=True):
+            temp_tuple.append((link["href"], link.text))
+        #self.debug_console(temp_tuple) 
+        for i in temp_tuple:
+            if i[1] == self.info["product"] or i[1] == self.info["color"]:
+                temp_link.append(i[0])
+        self.debug_console(temp_link)
+        #self.final_link = list(set([x for x in temp_link if temp_link.count(x) == 2]))
+        print(temp_link[0])
+    
+    
 if __name__ == "__main__":
     INFO = {
-            "driver": "geckodriver",
-            "product": "Thermal Zip Up Hooded Sweatshirt",
-            "color": "Tangerine",
+            "product": "S/S Pocket Tee",
+            "color": "Black",
             "size": "Medium",
-            "category": "sweatshirts",
+            "category": "tops_sweaters",
             "namefield": "example",
             "emailfield": "example@example.com",
             "phonefield": "XXXXXXXXXX",
@@ -36,3 +56,5 @@ if __name__ == "__main__":
             "year": "2020",
             "ccv": "123"
             }
+    bot = SupremeBot(True,**INFO)
+    bot.find_product()
